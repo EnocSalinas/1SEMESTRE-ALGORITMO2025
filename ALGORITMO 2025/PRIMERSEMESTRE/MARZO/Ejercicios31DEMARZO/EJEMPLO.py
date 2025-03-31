3f7dece29bfe69993aaaecc48368b00286dcd247
EnocSalinas <wenocms@uamv.edu.ni>
2025-03-31 16:39:58 -0600

    nueva carpeta ejercicios 31 de marzo

---
+++ ALGORITMO 2025/PRIMERSEMESTRE/MARZO/Ejercicios31DEMARZO/EJEMPLO.py
@@ -0,0 +1,14 @@
+
+def suma(a,b):
+    return a + b
+
+def resta(a,b):
+    return a - b
+
+print("Ejemplificando funciones en Python")
+num1=int(input("Digita el primer número"))
+num2=int(input("Digita el segundo número"))
+resp=suma(num1,num2)
+print("La suma es:",resp)
+resp=resta(num1,num2)
+print("La resta es:",resp)
---
+++ ALGORITMO 2025/PRIMERSEMESTRE/MARZO/Ejercicios31DEMARZO/PRACTICA.py
@@ -0,0 +1,13 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+num1 = int(input("Digita el primer número: "))
+num2 = int(input("Digita el segundo número: "))
+resp = suma(num1, num2)
+print("La suma es:", resp)
+resp = resta(num1, num2)
+print("La resta es:", resp)
+print("Ejemplificando funciones en Python. La Suma y Resta se realizo correctamente")
